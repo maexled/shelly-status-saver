5c5f6899486c04f275dd3ce9b3b12c5ede43b128
Max <max@dasmaexle.de>
2022-06-02 23:19:53 +0200

    added exceptions for connection errors in request

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import time
 import requests
 import json
 
-import threading
-
 from base import Base, session_factory
 
 from models.mystrom_device import MystromDevice
@@ -20,7 +18,18 @@ def get_devices():
     return device_query.all()
 
 def request_data_and_store(device):
-    response = requests.get(f'http://{device.ip}/report')
+    try:
+        response = requests.get(f'http://{device.ip}/report')
+    except requests.ConnectionError as e:
+        print(f'Device {device.name} with ip address {device.ip} seems to be not reachable.')
+        return
+    except requests.Timeout as e:
+        print(f'Request to device {device.name} with ip address {device.ip} timed out.')
+        return
+    except requests.RequestException as e:
+        print(f'Request to device {device.name} with ip address {device.ip} failed.')
+        return
+
     response = json.loads(response.text)
     mystrom_result = MystromResult(device_id=device.id, power=response["power"], ws=response["Ws"], relay=response["relay"], temperature=response["temperature"])
 
